a2e0c92bf4094c541afe75801e1cb04a3b1e4261
IshaanAhmad1 <24AHMADIs@hazelwick.org>
2026-09-22 08:50:40 +0000

    22/09/26

---
+++ python/9B/Cp2/Ishaan_Ahmad/Task15.py
@@ -0,0 +1,4 @@
+num1 = float(input("Give a width: "))
+num2 = float(input("Give a length: "))
+num3 = (2*num1) + (2*num2)
+print("The perimeter is: ", num3)
---
+++ python/9B/Cp2/Ishaan_Ahmad/Task9.py
@@ -0,0 +1,4 @@
+num1 = int(input("Give a number: "))
+for i in range (1,13):
+    num2 = num1*i
+    print(i, " times ", num1, " is ", num2)
